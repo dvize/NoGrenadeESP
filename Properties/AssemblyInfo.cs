354a4ae9fb7fd46ed56054f81245f7f3d76deca8
dvize <dvize404@gmail.com>
2024-04-09 17:36:10 -0700

    Update Version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -17,6 +17,6 @@ using VersionChecker;
 
 [assembly: Guid("8fdfc616-1704-4056-a87e-2783692cc153")]
 
-[assembly: AssemblyVersion("1.6.1.0")]
-[assembly: AssemblyFileVersion("1.6.1.0")]
+[assembly: AssemblyVersion("1.6.2.0")]
+[assembly: AssemblyFileVersion("1.6.2.0")]
 [assembly: TarkovVersion(29197)]
